664713286e8a46b467a5a6ce08a4709dd8211c3d
Marta Salvador <martasbsv@gmail.com>
2023-04-11 09:48:09 +0200

    week1day2

---
+++ src/d1/Main.java
@@ -0,0 +1,9 @@
+package d1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println(Marta you're the best);
+    }
+}
